ff0fb4b4b61936d8e2f64d498a51cac35ba0e9e8
Taras Grynik <grynik25.03@gmail.com>
2019-05-06 22:35:21 +0300

    added calculiator

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let margine = document.getElementById("real-margine"),
+lineHeightTop = document.getElementById("lineHeightTop"),
+fontSizeTop = document.getElementById("fontSizeTop"),
+lineHeightBottom = document.getElementById("lineHeightBottom"),
+fontSizeBottom = document.getElementById("fontSizeBottom"),
+btn = document.getElementById("btn");
+
+btn.onclick = function (num1, num2, num3, num4, num5) {
+  num1 = margine.value;
+  num2 = lineHeightTop.value;
+  num3 = fontSizeTop.value;
+  num4 = lineHeightBottom.value;
+  num5 = fontSizeBottom.value;
+
+  realMargine = num1 - ((num2 - num3) / 2) - ((num4 - num5) / 2);
+
+  alert(`Ваш реальний відступ мід текстом дорівнює: ${Math.round(realMargine)}!!!`);
+};
